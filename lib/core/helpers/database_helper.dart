26e336454d5a2bb4bfb72d7ea3fcc80ede3a7695
Carlos Ramirez <cr2503150@gmail.com>
2025-07-05 01:17:35 -0400

    Agregando database Helper para la base de dato local con sqflite

---
+++ lib/core/helpers/database_helper.dart
@@ -0,0 +1,38 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'todo_database.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE todos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        descripcion TEXT NOT NULL,
+        fechaLimite TEXT NOT NULL,
+        isCompleted INTEGER NOT NULL DEFAULT 0
+      )
+    ''');
+  }
+
+  Future<void> closeDatabase() async {
+    final db = await database;
+    await db.close();
+  }
+} 
--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,8 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:test_todo/core/cubit/theme/theme_cubit.dart';
+import 'package:test_todo/core/helpers/database_helper.dart';
 
 final sl = GetIt.instance;
 
 Future<void> serviceLocator() async {
   sl.registerLazySingleton<ThemeCubit>(() => ThemeCubit());
+
+  sl.registerLazySingleton<DatabaseHelper>(() => DatabaseHelper());
 }

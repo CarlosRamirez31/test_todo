503c137d74185b3394bb1a4b884dde7019749393
Carlos Ramirez <cr2503150@gmail.com>
2025-07-05 02:09:52 -0400

    Inyectando datasource, repositorio y cubit

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,6 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:test_todo/core/cubit/theme/theme_cubit.dart';
 import 'package:test_todo/core/helpers/database_helper.dart';
+import 'package:test_todo/domain/repositories/todo_repository.dart';
+import 'package:test_todo/infrastructure/datasources/todo_datasource.dart';
+import 'package:test_todo/infrastructure/repositories/todo_repository_impl.dart';
+import 'package:test_todo/presentation/todo/cubits/get_todo/get_todo_cubit.dart';
 
 final sl = GetIt.instance;
 
@@ -8,4 +12,13 @@ Future<void> serviceLocator() async {
   sl.registerLazySingleton<ThemeCubit>(() => ThemeCubit());
 
   sl.registerLazySingleton<DatabaseHelper>(() => DatabaseHelper());
+
+  // Datasource
+  sl.registerLazySingleton<TodoDatasource>(() => TodoDatasourceImpl(sl()));
+
+  // Repository
+  sl.registerLazySingleton<TodoRepository>(() => TodoRepositoryImpl(sl()));
+
+  //Cubits
+  sl.registerFactory<GetTodoCubit>(() => GetTodoCubit(sl()));
 }

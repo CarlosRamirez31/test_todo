00f1b749f3e7cce21b4bf77c9acc1bca880ae56d
Carlos Ramirez <cr2503150@gmail.com>
2025-07-05 11:24:52 -0400

    Modificando app theme

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primaryColor = Color(0xFF393433);
   
   static ThemeData get lightTheme => ThemeData(
     useMaterial3: true,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: primaryColor,
+      seedColor: Colors.blue,
       brightness: Brightness.light,
     ),
   );
@@ -14,7 +13,7 @@ class AppTheme {
   static ThemeData get darkTheme => ThemeData(
     useMaterial3: true,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: primaryColor,
+      seedColor: Colors.blue,
       brightness: Brightness.dark,
     ),
   );
